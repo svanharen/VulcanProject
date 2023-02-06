c1708fbcb0071cc916dd1ff144c8f96eeed6eb39
Catherine Addison <caddison4@my.bcit.ca>
2023-02-06 11:23:14 -0800

    added hamburger functionality

--- dev/scripts/script.js
+++ dev/scripts/script.js
@@ -1,1 +1,52 @@
 // Write your JavaScript here...
+// JavaScript Document
+
+// Responsive Menu - Dropdown
+const body = document.body;
+const btnMenu = document.getElementById("btn-menu");
+const bar = document.getElementById("bar");
+const nav = document.getElementById("main-navigation");
+
+btnMenu.addEventListener("click", openMenu);
+// Prevents the focus state from activating
+btnMenu.addEventListener("mousedown", function (e) {
+	e.preventDefault();
+});
+
+function openMenu() {
+	body.classList.toggle("show");
+	nav.classList.add("activated");
+	body.classList.add("animating");
+}
+
+// Create a media query list using
+// matchMedia
+const mql = window.matchMedia("(min-width: 768px)");
+
+// Add a Media Query Listener to the
+// above media query list
+mql.addListener(removeTransition);
+
+// Function to remove the transition
+// from the navigation when the user
+// resizes the browser to desktop
+// sizes. In this case when the
+// screen width becomes wider ....
+function removeTransition(e) {
+	// e -> is the event object
+	// e.matches -> stores a true false
+	// value depending if the media query
+	// list set above matches or not
+	if (e.matches) {
+		body.classList.remove("show", "animating");
+		nav.classList.remove("activated");
+	}
+}
+
+// Keep overflow hidden on until the menu
+// transition has finished
+body.addEventListener("transitionend", function () {
+	if (!body.classList.contains("show")) {
+		body.classList.remove("animating");
+	}
+});
